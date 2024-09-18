440d479803bef3a527832194c0f7aa0493cc0a9a
edonthopo <120410310+edonthopo@users.noreply.github.com>
2024-09-18 23:37:11 +0700

    Add files via upload

---
+++ RPLBK Liskov Example.java
@@ -0,0 +1,50 @@
+
+class WritableFile {
+
+    public void write(String content) {
+
+        System.out.println("Writing: " + content);
+
+    }
+
+    public void read() {
+
+        System.out.println("Reading file...");
+
+    }
+
+}
+
+class ReadOnlyFile {
+
+    public void read() {
+
+        System.out.println("Reading file...");
+
+    }
+
+}
+
+
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        WritableFile writableFile = new WritableFile();
+
+        writableFile.write("This is a writable file");
+
+        writableFile.read();
+
+        
+
+        ReadOnlyFile readOnlyFile = new ReadOnlyFile();
+
+        readOnlyFile.read();
+
+        // Tidak ada metode write() di ReadOnlyFile, jadi tidak ada pelanggaran LSP
+
+    }
+
+}
